6e4427a0d3e1939e5cb10018a97264a501af10c7
Natascha Lamsu <n.lamsu@gmail.com>
2020-04-09 21:13:16 +0700

    initial commit server

---
+++ server/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const db = mongoose.connection;
+mongoose.connect('mongodb+srv://admin:admin@cluster0-1o9kj.mongodb.net/test?retryWrites=true&w=majority');
+
+// GET ROUTES HERE
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+db.on('error', console.error.bind(console, 'Connection Error: '));
+db.once('open', function() {
+    console.log('---Mongoose Connected---');
+});
+
+app.get('/', (req, res) => {
+    res.send('mongoose CRUD');
+});
+
+// USE ROUTES HERE
+
+app.listen(3000, (connect => {
+    console.log('--- CONNECTED ---');
+}));
